f7ded6eb88cbc4d5a55e145022e5b61c70f0f294
Luan <barkon32@tuta.io>
2023-04-21 21:44:04 -0300

    finalizado criaćão de testes de warehouses

--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -10,4 +10,25 @@ describe 'usuário visita tela inicial' do
     # Assert
     expect(page).to have_content('Galpões & Estoque')
   end
+
+  it 'e vê os galpões cadastrados' do 
+    # Arrange
+    # cadastrar 2 galpões: Rio e Maceio
+    Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
+    Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+
+    # Act
+    visit ('/')
+
+    # Assert
+    expect(page).to have_content('Rio')
+    expect(page).to have_content('Código: SDU')
+    expect(page).to have_content('Cidade: Rio de Janeiro')
+    expect(page).to have_content('60000 m2')
+
+    expect(page).to have_content('Maceio')
+    expect(page).to have_content('Código: MCZ')
+    expect(page).to have_content('Cidade: Maceio')
+    expect(page).to have_content('50000 m2')
+  end
 end
