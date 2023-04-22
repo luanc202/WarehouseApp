7b0348d921930970d3f947870025df9b258f42d2
Luan <barkon32@tuta.io>
2023-04-21 21:14:46 -0300

    finalizado primeiro teste inicial

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,7 +30,7 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
-  config.before(:type :system) do
+  config.before(type: :system) do
     driven_by(:rack_test)
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
