6cb4930137fbeb5127606462e081ebc50e03ac10
Luan <barkon32@tuta.io>
2023-05-01 18:09:50 -0300

    formatado código

--- Warehouse-app/spec/system/user_delete_warehouse_spec.rb
+++ Warehouse-app/spec/system/user_delete_warehouse_spec.rb
@@ -4,12 +4,12 @@ describe 'Usuário remove um galpão' do
   it 'com sucesso' do
     # Arrange
     Warehouse.create!(name: 'Cuiabá', code: 'CWB', city: 'Cuiabá', area: 70_000,
-      address: 'Av das Capivaras, 143', cep: '7550-000', description: 'Galpão Central Brasileiro')
+                      address: 'Av das Capivaras, 143', cep: '7550-000', description: 'Galpão Central Brasileiro')
 
     # Act
-      visit root_path
-      click_on 'Cuiabá'
-      click_on 'Apagar'
+    visit root_path
+    click_on 'Cuiabá'
+    click_on 'Apagar'
 
     # Assert
     expect(current_path).to eq(root_path)
@@ -22,14 +22,14 @@ describe 'Usuário remove um galpão' do
     # Arrange
 
     first_warehouse = Warehouse.create!(name: 'Cuiabá', code: 'CWB', city: 'Cuiabá', area: 70_000,
-      address: 'Av das Capivaras, 143', cep: '7550-000', description: 'Galpão Central Brasileiro')
+                                        address: 'Av das Capivaras, 143', cep: '7550-000', description: 'Galpão Central Brasileiro')
     second_warehouse = Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000,
-      address: 'Avenida do Museu do Amanhã, 1000', cep: '2100-000', description: 'Galpão Rio')
+                                         address: 'Avenida do Museu do Amanhã, 1000', cep: '2100-000', description: 'Galpão Rio')
 
-      # Act
-      visit root_path
-      click_on 'Cuiabá'
-      click_on 'Apagar'
+    # Act
+    visit root_path
+    click_on 'Cuiabá'
+    click_on 'Apagar'
 
     # Assert
     expect(current_path).to eq(root_path)
@@ -37,5 +37,5 @@ describe 'Usuário remove um galpão' do
     expect(page).not_to have_content('Cuiabá')
     expect(page).not_to have_content('CWB')
     expect(page).to have_content('Rio')
-    end
+  end
 end
