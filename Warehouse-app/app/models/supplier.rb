6e47f231b2f956693fa1e6e5a1bd4587f3ba7db8
Luan <barkon32@tuta.io>
2023-05-01 22:14:47 -0300

    feita criaćão do modelo e schema de supplier

---
+++ Warehouse-app/app/models/supplier.rb
@@ -0,0 +1,7 @@
+class Supplier < ApplicationRecord
+  validates :corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email, presence: true
+
+  validates :state, length: { is: 2 }
+
+  validates :registration_number, uniqueness: true
+end
